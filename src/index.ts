0a41e3525837f0a79ca894f84d2c2e43c49bf883
8ctopus <hello@octopuslabs.io>
2022-02-20 20:17:25 +0500

    improved: example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import {hello} from "./module";
 
 hello("8ctopus", 10);
+hello("squid", 5);
+hello("babetta", 5);
+hello("roxy", 8);
+
 //hello(123);
--- src/module.ts
+++ src/module.ts
@@ -1,3 +1,4 @@
 export function hello(name: string, age: number) {
-    return `hello ${name}, you are ${age} years old.`
+    const message = `hello ${name}, you are ${age} years old.`;
+    console.debug(message);
 }
